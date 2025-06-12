3ed54688e07987bffb44eb30aba5d93e8fd13d7a
Uday <budayteja009@gmail.com>
2025-06-12 21:45:53 +0530

    Add eureka server

---
+++ eureka-sever/src/main/java/com/app/eurekasever/EurekaSeverApplication.java
@@ -0,0 +1,13 @@
+package com.app.eurekasever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSeverApplication.class, args);
+    }
+
+}
