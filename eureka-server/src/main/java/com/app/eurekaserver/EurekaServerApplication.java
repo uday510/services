3fe3c9e17039d22d1ef01f73e19ded8c94ab3669
Ubuntu <udayteja01@azure-vm.554ktj5o3l4urd5uxwbt5mxjog.zx.internal.cloudapp.net>
2025-06-14 05:28:36 +0000

    add codes:

--- eureka-server/src/main/java/com/app/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/app/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.app.eurekasever;
+package com.app.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaSeverApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaSeverApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
