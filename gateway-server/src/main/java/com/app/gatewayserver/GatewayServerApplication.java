d3c33b4738e0eec8eae08448cb60869f0b060dc6
Ubuntu <udayteja01@azure-vm.554ktj5o3l4urd5uxwbt5mxjog.zx.internal.cloudapp.net>
2025-06-15 08:19:26 +0000

    rename all the services

--- gateway-server/src/main/java/com/app/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/app/gatewayserver/GatewayServerApplication.java
@@ -2,6 +2,9 @@ package com.app.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
@@ -10,4 +13,22 @@ public class GatewayServerApplication {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+
+			.route("accounts-service", r -> r.path("/app/accounts-service/**")
+				.filters(f -> f.rewritePath("/app/accounts-service/(?<segment>.*)", "/${segment}"))
+				.uri("lb://ACCOUNTS"))
+
+			.route("cards-service", r -> r.path("/app/cards-service/**")
+				.filters(f -> f.rewritePath("/app/cards-service/(?<segment>.*)", "/${segment}"))
+				.uri("lb://CARDS"))
+
+			.route("loans-service", r -> r.path("/app/loans-service/**")
+				.filters(f -> f.rewritePath("/app/loans-service/(?<segment>.*)", "/${segment}"))
+				.uri("lb://LOANS"))
+
+			.build();
+	}
 }
