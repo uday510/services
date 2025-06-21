40ce6a9249d639229f6f5d256332926efced2dd4
Uday Teja <dev@udayteja.com>
2025-06-21 15:38:48 +0000

    add codes

--- accounts/src/main/java/com/app/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/app/accounts/controller/AccountsController.java
@@ -29,9 +29,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
-@Tag(name = "CRUD REST APIs for Accounts", description = "CRUD APIs: CREATE, UPDATE, FETCH AND DELETE")
+@Tag(name = "CRUD REST APIs for Accounts",
+                description = "CRUD APIs: CREATE, UPDATE, FETCH AND DELETE")
 @RestController
-@RequestMapping(value = "/api/accounts", produces = { MediaType.APPLICATION_JSON_VALUE })
+@RequestMapping(value = "/api/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class AccountsController {
 
@@ -48,8 +49,7 @@ public class AccountsController {
 
         public AccountsController(
 
-                        IAccountService iAccountService,
-                        Environment environment,
+                        IAccountService iAccountService, Environment environment,
                         AccountsContactInfoDto accountsContactInfoDto) {
 
                 this.iAccountService = iAccountService;
@@ -57,30 +57,29 @@ public class AccountsController {
                 this.accountsContactInfoDto = accountsContactInfoDto;
         }
 
-        @Operation(summary = "Create Account REST API", description = "REST API to create new Customer & Account inside Accounts Service")
+        @Operation(summary = "Create Account REST API",
+                        description = "REST API to create new Customer & Account inside Accounts Service")
         @ApiResponse(responseCode = "201", description = "HTTP Status CREATED")
         // @PostMapping(path = {"", "/"})
         @PostMapping
         public ResponseEntity<SuccessResponseDto<String>> createAccount(
-                        @Valid @RequestBody CustomerDto customerDto,
-                        HttpServletRequest request) {
+                        @Valid @RequestBody CustomerDto customerDto, HttpServletRequest request) {
                 iAccountService.createAccount(customerDto);
 
-                SuccessResponseDto<String> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.CREATED,
-                                HttpStatus.CREATED.getReasonPhrase(),
-                                null);
+                SuccessResponseDto<String> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.CREATED, HttpStatus.CREATED.getReasonPhrase(), null);
 
                 return ResponseEntity.status(HttpStatus.CREATED).body(response);
         }
 
-        @Operation(summary = "Fetch Account REST API", description = "REST API to fetch Customer & Account inside Accounts Service")
+        @Operation(summary = "Fetch Account REST API",
+                        description = "REST API to fetch Customer & Account inside Accounts Service")
         @ApiResponse(responseCode = "200", description = "HTTP Status OK")
         // @GetMapping(path = {"", "/"} )
         @GetMapping
-        public ResponseEntity<SuccessResponseDto<CustomerDto>> fetchAccount(
-                        @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber,
+        public ResponseEntity<SuccessResponseDto<CustomerDto>> fetchAccount(@RequestParam @Pattern(
+                        regexp = "(^$|[0-9]{10})",
+                        message = "Mobile number must be 10 digits") String mobileNumber,
                         HttpServletRequest request) {
 
                 CustomerDto customerDto = iAccountService.fetchAccount(mobileNumber);
@@ -92,47 +91,45 @@ public class AccountsController {
                 // Thread.currentThread().interrupt();
                 // }
 
-                SuccessResponseDto<CustomerDto> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
-                                customerDto);
+                SuccessResponseDto<CustomerDto> response =
+                                SuccessResponseDto.of(request.getRequestURI(), HttpStatus.OK,
+                                                HttpStatus.OK.getReasonPhrase(), customerDto);
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        @Operation(summary = "Update Account REST API", description = "REST API to update Customer & Account inside Accounts Service")
-        @ApiResponses({
-                        @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
-                        @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
-        })
+        @Operation(summary = "Update Account REST API",
+                        description = "REST API to update Customer & Account inside Accounts Service")
+        @ApiResponses({@ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+                        @ApiResponse(responseCode = "500",
+                                        description = "HTTP Status Internal Server Error",
+                                        content = @Content(schema = @Schema(
+                                                        implementation = ErrorResponseDto.class)))})
         // @PutMapping(path = {"", "/"})
         @PutMapping
-        public ResponseEntity<SuccessResponseDto<Object>> updateAccount(@Valid @RequestBody CustomerDto dto,
-                        HttpServletRequest request) {
+        public ResponseEntity<SuccessResponseDto<Object>> updateAccount(
+                        @Valid @RequestBody CustomerDto dto, HttpServletRequest request) {
                 Map<String, Object> data = new HashMap<>();
                 data.put("status", "updated");
                 data.put("success", true);
 
                 iAccountService.updateAccount(dto);
-                SuccessResponseDto<Object> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
-                                data);
+                SuccessResponseDto<Object> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(), data);
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        @Operation(summary = "Delete Account & Customer Details REST API", description = "REST API to delete Customer & Account details based on a mobile number")
-        @ApiResponses({
-                        @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
-                        @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error")
-        })
+        @Operation(summary = "Delete Account & Customer Details REST API",
+                        description = "REST API to delete Customer & Account details based on a mobile number")
+        @ApiResponses({@ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+                        @ApiResponse(responseCode = "500",
+                                        description = "HTTP Status Internal Server Error")})
         // @DeleteMapping(path = {"", "/"})
         @DeleteMapping
-        public ResponseEntity<SuccessResponseDto<Object>> deleteAccount(
-                        @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber,
+        public ResponseEntity<SuccessResponseDto<Object>> deleteAccount(@RequestParam @Pattern(
+                        regexp = "(^$|[0-9]{10})",
+                        message = "Mobile number must be 10 digits") String mobileNumber,
                         HttpServletRequest request) {
                 Map<String, Object> data = new HashMap<>();
                 data.put("status", "deleted");
@@ -140,17 +137,15 @@ public class AccountsController {
 
                 iAccountService.deleteAccount(mobileNumber);
 
-                SuccessResponseDto<Object> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
-                                data);
+                SuccessResponseDto<Object> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(), data);
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
         @Retry(name = "getBuildInfo", fallbackMethod = "getBuildInfoFallback")
-        @Operation(summary = "Fetch Build Info REST API", description = "REST API to fetch Build information")
+        @Operation(summary = "Fetch Build Info REST API",
+                        description = "REST API to fetch Build information")
         @ApiResponse(responseCode = "200", description = "HTTP Status OK")
         @GetMapping("/build-info")
         public ResponseEntity<SuccessResponseDto<String>> getBuildInfo(HttpServletRequest request) {
@@ -166,76 +161,69 @@ public class AccountsController {
 
                 // throw new TimeoutException();
 
-                SuccessResponseDto<String> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
-                                buildVersion);
+                SuccessResponseDto<String> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(), buildVersion);
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        public ResponseEntity<SuccessResponseDto<String>> getBuildInfoFallback(HttpServletRequest request,
-                        Throwable throwable) {
+        public ResponseEntity<SuccessResponseDto<String>> getBuildInfoFallback(
+                        HttpServletRequest request, Throwable throwable) {
 
-                SuccessResponseDto<String> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
+                SuccessResponseDto<String> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(),
                                 HttpStatus.CONTINUE.toString());
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
         @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
-        @Operation(summary = "Fetch JAVA Version REST API", description = "REST API to fetch Java version information")
+        @Operation(summary = "Fetch JAVA Version REST API",
+                        description = "REST API to fetch Java version information")
         @ApiResponse(responseCode = "200", description = "HTTP Status OK")
         @GetMapping("/java-version")
-        public ResponseEntity<SuccessResponseDto<String>> getJavaVersion(HttpServletRequest request) {
+        public ResponseEntity<SuccessResponseDto<String>> getJavaVersion(
+                        HttpServletRequest request) {
 
-                SuccessResponseDto<String> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
+                SuccessResponseDto<String> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(),
                                 environment.getProperty("JAVA_HOME"));
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        @Operation(summary = "Fetch Maven Version REST API", description = "REST API to fetch Maven version information")
+        @Operation(summary = "Fetch Maven Version REST API",
+                        description = "REST API to fetch Maven version information")
         @ApiResponse(responseCode = "200", description = "HTTP Status OK")
         @GetMapping("/maven-home")
         public ResponseEntity<SuccessResponseDto<String>> getMavenHome(HttpServletRequest request) {
 
-                SuccessResponseDto<String> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
+                SuccessResponseDto<String> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(),
                                 environment.getProperty("MAVEN_HOME"));
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        @Operation(summary = "Fetch Contact Info REST API", description = "REST API to fetch Contact info information")
+        @Operation(summary = "Fetch Contact Info REST API",
+                        description = "REST API to fetch Contact info information")
         @ApiResponse(responseCode = "200", description = "HTTP Status OK")
         @GetMapping("/contact")
-        public ResponseEntity<SuccessResponseDto<AccountsContactInfoDto>> getContactInfo(HttpServletRequest request) {
+        public ResponseEntity<SuccessResponseDto<AccountsContactInfoDto>> getContactInfo(
+                        HttpServletRequest request) {
 
                 SuccessResponseDto<AccountsContactInfoDto> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
-                                accountsContactInfoDto);
+                                request.getRequestURI(), HttpStatus.OK,
+                                HttpStatus.OK.getReasonPhrase(), accountsContactInfoDto);
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        public ResponseEntity<SuccessResponseDto<String>> getJavaVersionFallback(HttpServletRequest request, Throwable throwable) {
+        public ResponseEntity<SuccessResponseDto<String>> getJavaVersionFallback(
+                        HttpServletRequest request, Throwable throwable) {
 
-                SuccessResponseDto<String> response = SuccessResponseDto.of(
-                                request.getRequestURI(),
-                                HttpStatus.OK,
-                                HttpStatus.OK.getReasonPhrase(),
+                SuccessResponseDto<String> response = SuccessResponseDto.of(request.getRequestURI(),
+                                HttpStatus.OK, HttpStatus.OK.getReasonPhrase(),
                                 "RATE_LIMIT_EXCEEDED");
 
                 return ResponseEntity.status(HttpStatus.OK).body(response);
